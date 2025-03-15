2edf8bd5c60bbdf041fb7ca9d814620f1cadf4e2
Thomas Pauli <thomas.pauli@stud.hslu.ch>
2025-03-15 15:22:35 +0100

    implement webserver add basic control elements

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-from uart.UartFactory import UartFactory
-from uart.UartSetup import UartSetup
-from uart.uart import Uart
-from protocol.kd3005p import Kd500p
-from pathlib import Path
-import time
+import os
+import logging
+from webserver import webserver
 
-def main():
-    print(f"Hello from kd3005p! current voltage is: {test}")
+log_level: str = os.getenv('LOG_LVL', "DEBUG")
+if log_level == "DEBUG":
+    log_level = logging.DEBUG
+else:
+    log_level = logging.INFO
+logging.basicConfig(level=log_level)
 
 
 if __name__ == "__main__":
-    path: Path = Path('uart/config/config_usb.json')
-    uart_setup: UartSetup = UartFactory.create_uart_setup(path)
-    serial: Uart = Uart(uart_setup)
-    serial.send_line("VSET1:20.5")
-    time.sleep(1)
-    serial.send_line("VSET1?")
-    test = serial.read_line()
-    kd500p: Kd500p = Kd500p(serial)
-    kd500p.set_output(1)
-    kd500p.set_ocp(1)
-    kd500p.get_status()
 
-    main()
+    webserver()
